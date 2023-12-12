26ab14b1d3ebbb0053da2cfd1da27c7e6d900e5d
Your NameVandita <you@example.comvanditakhare252@gmail.com>
2023-12-12 20:51:59 +0530

    'done'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  let t = new Date().toLocaleTimeString();
+  const [ctime, settime] = useState(t);
+
+  const UpdateTime = () => {
+    t = new Date().toLocaleTimeString();
+    settime(t);
+
+  };
+  setInterval(UpdateTime, 1000)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div><h1 >{t}</h1></div>
+
+    </>
   );
 }
 
